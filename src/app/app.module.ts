0944b9f1ed96258b30108c1b07d922f2884999aa
AntonVoronezh <pstut@mail.ru>
2020-02-05 18:13:17 +0300

    add files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
-import { AppComponent } from './app.component';
-import { SelectorComponent } from './selector/selector.component';
-import { CalendarComponent } from './calendar/calendar.component';
-import { OrganizerComponent } from './organizer/organizer.component';
+import {AppComponent} from './app.component';
+import {SelectorComponent} from './selector/selector.component';
+import {CalendarComponent} from './calendar/calendar.component';
+import {OrganizerComponent} from './organizer/organizer.component';
 import {MomentPipe} from './shared/moment.pipe';
 
 @NgModule({
@@ -16,9 +17,12 @@ import {MomentPipe} from './shared/moment.pipe';
     MomentPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/organizer/organizer.component.ts
+++ src/app/organizer/organizer.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {DateService} from '../shared/date.service';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-organizer',
@@ -7,10 +8,18 @@ import {DateService} from '../shared/date.service';
   styleUrls: ['./organizer.component.scss']
 })
 export class OrganizerComponent implements OnInit {
+  form: FormGroup;
 
-  constructor(private dateService:DateService) { }
-
-  ngOnInit() {
+  constructor(private dateService: DateService) {
   }
 
+  ngOnInit() {
+    this.form = new FormGroup({
+      title: new FormControl('', Validators.required)
+    });
+  }
+
+  submit() {
+    const {title} = this.form.value;
+  }
 }
