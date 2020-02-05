06ac62c643ec84603697331e54abc5669eceea99
AntonVoronezh <pstut@mail.ru>
2020-02-05 14:36:40 +0300

    add files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SelectorComponent } from './selector/selector.component';
+import { CalendarComponent } from './calendar/calendar.component';
+import { OrganizerComponent } from './organizer/organizer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SelectorComponent,
+    CalendarComponent,
+    OrganizerComponent
   ],
   imports: [
     BrowserModule
